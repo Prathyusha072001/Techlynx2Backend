03c4a94a3d59b8337c725dffdc66e69387f418bd
Prathyusha072001 <vsnprathyusha@gmail.com>
2025-10-03 12:08:15 +0530

    change in backend

--- src/config/db.js
+++ src/config/db.js
@@ -5,10 +5,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "prathyusha2001",
-  database: process.env.DB_NAME || "techlynxproject2",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
